6b58298dd08fb365a6aab468315752ff86f2bcbc
AmadeusWM <63149896+AmadeusWM@users.noreply.github.com>
2025-12-20 00:58:53 +0000

    Update release.nix to version 0.1.25

--- nix/release.nix
+++ nix/release.nix
@@ -1,7 +1,7 @@
 { lib }:
 
 {
-	version = "0.1.24";
-	electron_sha256 = "sha256-KNmlrQBYU8oHpoGWQRvrxskg77t51qV6XprWIdrFRqE=";
-	tauri_sha256 = "sha256-FNCJI9e2Kb0c47SwXlR5EphRcuEbISjZGBVhZx2OZW0=";
+	version = "0.1.25";
+	electron_sha256 = "sha256-8i42Yg2s8yUFjmmluxxVSRV9UCXSETX7zYtDcuasbeg=";
+	tauri_sha256 = "sha256-3xlz9Su+AWlXkT3WBvB5KILmoIVGezAObvPgkz5HJqI=";
 }
